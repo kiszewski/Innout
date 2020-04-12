0e6858e6dd30696c7c27bba11a57868e4ba250b6
Leo Kiszewski <kiszewski1999@gmail.com>
2020-04-12 14:41:51 -0300

    adicionado metodo getResultFromQuery na classe Database

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
-<h1>OK</h1>
-
 <?php
 require_once(dirname(__FILE__, 2) . '/src/database.php');
 
 Database::getConnection();
+
+$sql = "SELECT * FROM users";
+$result = Database::getResultFromQuery($sql);
+
+while ($row = $result->fetch_assoc()) {
+    print_r($row);
+
+}
--- src/database.php
+++ src/database.php
@@ -11,4 +11,12 @@ class Database {
             return $conn;
         }
     }
+
+    public static function getResultFromQuery($sql) {
+        $conn = self::getConnection();
+        $result = $conn->query($sql);
+        $conn->close();
+
+        return $result;
+    }
 }
