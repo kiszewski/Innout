349cb4850e8f7bc13a7afce64fff6d6c0169dd96
Leo Kiszewski <kiszewski1999@gmail.com>
2020-04-12 14:12:09 -0300

    first commit, criada conexão com banco de dados

---
+++ public/index.php
@@ -0,0 +1,6 @@
+<h1>OK</h1>
+
+<?php
+require_once(dirname(__FILE__, 2) . '/src/database.php');
+
+Database::getConnection();
---
+++ src/database.php
@@ -0,0 +1,14 @@
+<?php
+
+class Database {
+    public static function getConnection() {
+        $envPath = realpath(dirname(__FILE__) . '/../env.ini');
+        $env = parse_ini_file($envPath);
+        $conn = new mysqli($env['host'], $env['user'], $env['password'], $env['database']);
+        if ($conn->connect_error) {
+            die('Erro: ' . $conn->connect_error);
+        } else {
+            return $conn;
+        }
+    }
+}
