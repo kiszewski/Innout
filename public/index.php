c46191ac6f5e03398819988a7753197eb20a21d5
Leo Kiszewski <kiszewski1999@gmail.com>
2020-04-12 18:55:46 -0300

    alterado metodos especiais na classe Models

--- public/index.php
+++ public/index.php
@@ -1,12 +1,7 @@
 <?php
-require_once(dirname(__FILE__, 2) . '/src/database.php');
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/models/User.php');
 
-Database::getConnection();
-
-$sql = "SELECT * FROM users";
-$result = Database::getResultFromQuery($sql);
-
-while ($row = $result->fetch_assoc()) {
-    print_r($row);
-
-}
+$user = new User(['nome' => "Leonardo", "email" => "leo@email.com"]);
+$user->nome = "Léo";
+echo $user->nome;
