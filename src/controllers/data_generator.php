00f02bac9df9f17e33e20f288f00d326680c99a5
Leo Kiszewski <kiszewski1999@gmail.com>
2020-04-19 17:56:52 -0300

    adicionado funcionalidade de popular a tabela working hours

--- src/controllers/data_generator.php
+++ src/controllers/data_generator.php
@@ -1,4 +1,9 @@
 <?php
+loadModel('WorkingHours');
+
+Database::executeSql('DELETE FROM working_hours');
+// Database::executeSql('DELETE FROM users where id > 5');
+
 function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate) {
     $regularHourDayTime = [
         'time1' => '08:00:00',
@@ -31,3 +36,24 @@ function getDayTemplateByOdds($regularRate, $extraRate, $lazyRate) {
         return $lazyHourDayTime;
     }
 }
+
+function populateWorkingHours($UserId, $initialDate, $regularRate, $extraRate, $lazyRate) {
+    $currentDate = $initialDate;
+    $today = new DateTime();
+    $columns = ['user_id' => $UserId, 'work_date' => $initialDate];
+
+    while(isBefore($currentDate, $today)) {
+        if(!isWeekend($currentDate)) {
+            $template = getDayTemplateByOdds($regularRate, $extraRate, $lazyRate);
+            $columns = array_merge($columns, $template);
+            $workingHours = new WorkingHours($columns);
+            $workingHours->save();
+        }
+        $currentDate = getNextDay($currentDate)->format('Y-m-d');
+        $columns['work_date'] = $currentDate;
+    }
+}
+
+populateWorkingHours(1, date('Y-m-1'), 70, 20, 10);
+
+echo "td certo";
