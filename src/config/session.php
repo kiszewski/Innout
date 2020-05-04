2fb0bdd6a445dc577e6107735d85dca89dd792dc
Leo Kiszewski <kiszewski1999@gmail.com>
2020-05-04 15:41:58 -0300

    projeto finalizado

--- src/config/session.php
+++ src/config/session.php
@@ -1,9 +1,14 @@
 <?php
 
-function requireValidateSession() {
+function requireValidateSession($requiresAdmin = false) {
     $user = $_SESSION['user'];
     if(!isset($user)) {
         header("Location: login.php");
+        exit();
+    }
+    if($requiresAdmin && !$user->is_admin) {
+        addErrorMsg("Acesso negado");
+        header("Location: day_records.php");
         exit();
     }
 }
--- src/controllers/manager_report.php
+++ src/controllers/manager_report.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-requireValidateSession();
+requireValidateSession(true);
 
 $activeUsersCount = User::getActiveUsersCount();
 
